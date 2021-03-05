49571ed54bb177a9cff8b7aaaba61b06d2fe53d6
sgamage3 <sgamage3@gatech.edu>
2021-03-05 01:33:57 -0500

    Invalidate all queries when reseting at the error screen

--- src/screens/Error/ErrorView.js
+++ src/screens/Error/ErrorView.js
@@ -1,10 +1,16 @@
 import { CommonActions } from '@react-navigation/native';
 import React from 'react';
 import { Button, Text } from 'react-native';
+import { useQueryClient } from 'react-query';
 import styled from 'styled-components/native';
 
 function ErrorScreen({ navigation }) {
+  const queryClient = useQueryClient();
+
   const handleReset = () => {
+    // Invalidate all query caches
+    queryClient.invalidateQueries(); // invalidate all queries in cache
+
     // Reset the navigation state and try again
     navigation.dispatch((state) => {
       return CommonActions.reset({
