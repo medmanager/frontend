dbff283a170d3c04b1092b060d3a6540be880080
sgamage3 <sgamage3@gatech.edu>
2021-03-05 01:32:26 -0500

    Convert dosage list to sectioned list

--- src/screens/Home/HomeView.js
+++ src/screens/Home/HomeView.js
@@ -1,14 +1,18 @@
 import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
 import React, { Fragment, useRef, useState } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, SectionList } from 'react-native';
 import styled from 'styled-components/native';
 import shallow from 'zustand/shallow';
 import FloatingAddMedicationButton from '../../components/FloatingAddMedicationButton';
 import { useAuth } from '../../store/useAuth';
 import useOccurrences from '../../store/useOccurrences';
+import { Colors } from '../../utils';
 import DosageListItem, {
   DosageListItemPlaceholder,
 } from './components/DosageListItem';
+
+dayjs.extend(isBetween);
 
 function HomeScreen() {
   const now = useRef(new Date());
@@ -29,7 +33,7 @@ function HomeScreen() {
       <SafeArea>
         <Container>
           <Header>{dayjs(now.current).format('dddd, MMMM D, YYYY')}</Header>
-          <DosageList
+          <FlatList
             data={new Array(5)}
             keyExtractor={(_, index) => index.toString()}
             renderItem={() => <DosageListItemPlaceholder />}
@@ -59,14 +63,52 @@ function HomeScreen() {
     );
   }
 
+  const sectionsData = [
+    {
+      title: 'Morning Meds',
+      data: [],
+    },
+    {
+      title: 'Afternoon Meds',
+      data: [],
+    },
+    {
+      title: 'Evening Meds',
+      data: [],
+    },
+    {
+      title: 'Nightime Meds',
+      data: [],
+    },
+  ];
+  occurrences[today].forEach((occurrence) => {
+    const scheduledTime = dayjs(occurrence.date);
+    if (scheduledTime.isBetween('5:00 AM', '9:59 AM')) {
+      sectionsData[0].data.push(occurrence);
+    } else if (scheduledTime.isBetween('10:00 AM', '2:59 PM')) {
+      sectionsData[1].data.push(occurrence);
+    } else if (scheduledTime.isBetween('3:00 PM', '7:59 PM')) {
+      sectionsData[2].data.push(occurrence);
+    } else {
+      sectionsData[3].data.push(occurrence);
+    }
+  });
+
+  console.log(sectionsData);
+
   return (
     <SafeArea>
       <Container>
         <Header>{dayjs(now.current).format('dddd, MMMM D, YYYY')}</Header>
-        <DosageList
-          data={occurrences[today]}
-          keyExtractor={(item, index) => index.toString()}
+        <SectionList
+          sections={sectionsData}
+          keyExtractor={(item, index) => item + index}
           renderItem={({ item }) => <DosageListItem {...item} />}
+          renderSectionHeader={({ section: { title, data } }) => {
+            if (data.length) {
+              return <SectionHeader>{title}</SectionHeader>;
+            }
+          }}
           onRefresh={() => refetch()}
           refreshing={isFetching}
         />
@@ -77,8 +119,9 @@ function HomeScreen() {
 
 const Header = styled.Text`
   font-size: 24px;
-  font-weight: 700;
-  margin-bottom: 8px;
+  margin-bottom: 24px;
+  text-align: center;
+  color: ${Colors.blue[500]};
 `;
 
 const Container = styled.View`
@@ -86,10 +129,13 @@ const Container = styled.View`
   padding: 24px;
 `;
 
-const DosageList = styled(FlatList)``;
-
 const SafeArea = styled.SafeAreaView`
   flex: 1;
+`;
+
+const SectionHeader = styled.Text`
+  font-size: 16px;
+  color: ${Colors.gray[600]};
 `;
 
 const Text = styled.Text`
