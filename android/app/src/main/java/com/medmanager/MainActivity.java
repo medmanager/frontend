98e3168b7952f79699ac1613ed47acd0b255256d
samgamage <sammygamage@gmail.com>
2021-04-14 17:37:16 -0400

    Add app icon and splash screen

--- android/app/src/main/java/com/medmanager/MainActivity.java
+++ android/app/src/main/java/com/medmanager/MainActivity.java
@@ -1,15 +1,27 @@
 package com.medmanager;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "MedManager";
-  }
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "MedManager";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+SplashScreen.show(this, R.style.SplashScreenTheme);        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+SplashScreen.show(this, R.style.SplashScreenTheme);    }
 }
