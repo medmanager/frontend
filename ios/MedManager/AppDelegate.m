23eb90dbebffa3937e74f4d81519411633ef3c2a
samgamage <sammygamage@gmail.com>
2021-04-22 20:20:24 -0400

    Remove unused depedencies
    * Firebase configuration files are no longer required to run the app

--- ios/MedManager/AppDelegate.m
+++ ios/MedManager/AppDelegate.m
@@ -3,7 +3,6 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import <Firebase.h>
 #import <UserNotifications/UserNotifications.h>
 #import <RNCPushNotificationIOS.h>
 
@@ -30,8 +29,6 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [FIRApp configure];
-
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
